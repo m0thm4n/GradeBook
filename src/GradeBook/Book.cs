a9106289e0b18b12f6b9efdd31bf2c3a2bd3fdc9
Nathan <nateim3@gmail.com>
2020-04-03 19:10:12 -0400

    Refactored Statistics class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -70,7 +70,20 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            Statistics result = new Statistics();
+            
+            using (StreamReader reader = File.OpenText($"{Name}.txt"))
+            {
+                string line = reader.ReadLine();
+                while (line != null) 
+                {
+                    double number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
 
@@ -132,35 +145,10 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             Statistics result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
             for (int index = 0; index < grades.Count; index++)
             {
-                result.Low = Math.Min(grades[index], result.Low);
-                result.High = Math.Max(grades[index], result.High);
-                result.Average += grades[index];
-            };
-            result.Average /= grades.Count;
-
-            switch(result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
+                result.Add(grades[index]);
             }
 
             return result;
